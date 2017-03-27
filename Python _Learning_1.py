b6fa7b5299f8ffc46827d5e8783f302de52d8429
Waxyoung <waxyoung@WaxyoungdeMacBook-Air.local>
2017-03-27 09:41:12 +0800

    Some notes

--- Python _Learning_1.py
+++ Python _Learning_1.py
@@ -1,12 +1,16 @@
 import argparse
 from PIL import Image
 
+#此处是采用argparse，直接在doc界面进行参数输入
 parser=argparse.ArgumentParser()
 parser.add_argument('file')
+#设置图像的输入路径
 parser.add_argument('-o','--output')
+#设置最后文件的输出路径，此处进行了参数简化，用-o代替-output
 parser.add_argument('--width',type=int,default=100)
 parser.add_argument('--height',type=int,default=80)
 args=parser.parse_args()
+#第5，12行是固定的，通过arg来接受参数
 
 IMG=args.file
 WIDTH=args.width
@@ -22,6 +26,7 @@ def get_char(r,g,b,alpha=256):
 
 if __name__=='__main__':
 
+#如果在终端运行此程序，则进行下列程序
 	img=Image.open(IMG)
 	img=img.resize((WIDTH,HEIGHT),Image.NEAREST)
 
@@ -31,7 +36,10 @@ if __name__=='__main__':
 		for j in range(WIDTH):
 			txt += get_char(*img.getpixel((j,i)))
 		txt += '\n'
+#对txt里加子符的方法可以借鉴一下
 
+#下面的部分是对字符串txt进行文件写入，并输出
+#用with的方法的好处是替代了try exception finally
 	print(txt)
 	if OUTPUT:
 		with open(OUTPUT,'w') as f:
